44a44742794e3fd69056dce1706c742e529fa090
andres diaz <andres.diaz@foonkiemonkey.co.uk>
2021-12-20 17:27:25 +0000

    Add task - 5 (Delete, improve all)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        temp = {}
+        for key, value in self.__objects.items():
+            if type(value) == cls:
+                temp[key] = value
+        return temp
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +51,15 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete a object from __objects"""
+        if obj is None:
+            return
+        else:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
