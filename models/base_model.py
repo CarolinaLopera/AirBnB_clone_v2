ec78d90440813f5842a17a3e10e6c176e98e6058
andres diaz <andres.diaz@foonkiemonkey.co.uk>
2021-12-21 01:53:22 +0000

    first implementation base model

--- models/base_model.py
+++ models/base_model.py
@@ -17,21 +17,17 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        from models import storage
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        if 'updated_at' in kwargs:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-        if 'created_at' in kwargs:
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-
-        if '__class__' in kwargs:
-            del kwargs['__class__']
-        self.__dict__.update(kwargs)
+        if kwargs:
+            if self.id is None:
+                setattr(self, 'id', str(uuid.uuid4()))
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
