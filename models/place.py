5021c5af294b5fd2e2413f83ef9aed34d3e54a0d
andres diaz <andres.diaz@foonkiemonkey.co.uk>
2021-12-22 20:30:36 +0000

    Task - 10

--- models/place.py
+++ models/place.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import os
 import models
+from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from models.review import Review
-from sqlalchemy import ForeignKey, Column, String, Integer, Float
+from sqlalchemy import ForeignKey, Column, String, Integer, Float, Table
 from sqlalchemy.orm import relationship
+
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60), ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False)
+                      )
 
 
 class Place(BaseModel, Base):
@@ -21,14 +32,29 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     amenity_ids = []
-
     reviews = relationship("Review", backref="place", cascade="all, delete")
+    amenities = relationship("Amenity", secondary=place_amenity,
+                             viewonly=False)
+    if os.getenv("HBNB_TYPE_STORAGE", None) != "db":
+        @property
+        def reviews(self):
+            """Getter reviews"""
+            review = []
+            for i in list(models.storage.all(Review).values()):
+                if i.place_id == self.id:
+                    review .append(i)
+            return review
 
-    @property
-    def reviews(self):
-        """Getter reviews"""
-        review = []
-        for i in list(models.storage.all(Review).values()):
-            if i.place_id == self.id:
-                revies .append(i)
-        return review
+        @property
+        def amenities(self):
+            """Getter amenities"""
+            amenity = []
+            for i in list(models.storage.all(Amenity).values()):
+                if i.id == self.amenity_ids:
+                    amenity.append(i)
+            return amenity
+
+        @amenities.setter
+        def amenities(self, obj):
+            if type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -12,15 +13,13 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
-
-    @property
-    def cities(self):
-        """ File storage city getter """
-        city_list = models.storage.all(City)
-        temp = []
-
-        for city in list(city_list.values()):
-            if city.state_id == self.id:
-                temp.append(city)
-
-        return temp
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """ File storage city getter """
+            city_list = models.storage.all(City)
+            temp = []
+            for city in list(city_list.values()):
+                if city.state_id == self.id:
+                    temp.append(city)
+            return temp
